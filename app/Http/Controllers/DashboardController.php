2e14f545c0f1b846a0bd517cbf98dec30675f276
Antonio Sousa <antonioufpi@gmail.com>
2024-06-30 23:14:02 -0300

    PROF-11: corrigindo o problema de N+1 nas consultas do banco de dados

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,8 +9,16 @@ class DashboardController extends Controller
 {
     public function __invoke(): View
     {
+        $userId = auth()->user()->id;
+
         return view('dashboard', [
-            'perguntas' => Pergunta::all(),
+            'perguntas' => Pergunta::withSum('votos', 'voto')
+                ->withCount([
+                    'votos as votos_count' => function ($query) use ($userId) {
+                        $query->where('user_id', $userId)->where('voto', 1);
+                    },
+                ])
+                ->get(),
         ]);
     }
 }
